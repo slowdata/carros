33d0f9c586e8764e9c1a4bb6c7c210086d98a92e
Ricardo Dias <slo.motion@gmail.com>
2021-05-19 17:53:11 +0100

    Some space

--- pages/components/Card.js
+++ pages/components/Card.js
@@ -2,6 +2,7 @@ const Card = ({ carro }) => {
     return (
         <div className="flex flex-col w-full h-36 p-4 bg-blue-100 border border-blue-400 rounded-lg">
             <h3>{carro.marca}</h3>
+
             <ul>
                 <li>{carro.matricula}</li>
                 <li>{carro.kms}</li>
