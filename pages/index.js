f96fcd1b7498d4a76c2aae12cb461e9eb97b72f9
Ricardo Dias <slo.motion@gmail.com>
2021-05-19 19:11:07 +0100

    one more test

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Index() {
     return (
-        <div className="min-h-screen flex justify-center items-center">
+        <div className="min-h-screen flex flex-col justify-center items-center">
             <Link href="/carros">
                 <a>
                     <h1 className="text-4xl uppercase">carro</h1>
